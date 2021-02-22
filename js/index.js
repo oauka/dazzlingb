c0a94e37643f2688ffe372a5a326c13bf599c212
oauka <77268885+oauka@users.noreply.github.com>
2021-02-22 13:09:12 +0900

    ver 2.0

--- js/index.js
+++ js/index.js
@@ -1,37 +1,129 @@
-$(".article1 .slide_group").slick({
+$(".slide_group").slick({
     autoplay: true, // 자동재생
     autoplaySpeed: 3000, // 간격시간
     dots: true, // 동그라미버튼
-    speed: 600, // 바뀌는시간(생략가능)
-    slidesToShow: 1, // 보여질슬라이드수(생략가능)
-    slidesToScroll: 1, // 이동슬라이드수(생략가능)
-    pauseOnHover: true, // 마우스오버시 멈춤여부(생략가능)
-    pauseOnDotsHover: true, // 동그라미번호버튼에 호버시 멈춤여부(생략가능)
     pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
-    cssEase: 'linear', // 속도함수(생략가능)
-    draggable: true, // 마우스드래그시 슬라이드 교체가능여부(생략가능)
-    fade: false, // 슬라이드가 수평으로 이동하지 않고, 제자리에서 사라지고 나타남(생략가능)
-    arrows: true, // 좌우화살표 사용여부(생략가능)
-    prevArrow: '<button class="prev"><i class="fas fa-angle-double-left"></i></button>',
-    nextArrow: '<button class="next"><i class="fas fa-angle-double-right"></i></button>',
-    responsive:[{
+    prevArrow: '<button class="prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="next"><i class="fas fa-angle-right"></i></button>',
+    responsive: [{
         breakpoint:1025,
-        settings:{
-            arrows: false
+        settings: {
+            arrows:false
         }
     }]
 })
 
-var elPlaystop = document.querySelector('.article1 .playstop')
-var ibtn = elPlaystop.childNodes
-elPlaystop.addEventListener('click', function(){
-    if ( ibtn[0].classList.contains('fa-pause') ) {
-        $('.article1 .slide_group').slick('slickPause')
-        ibtn[0].classList.remove('fa-pause')
-        ibtn[0].classList.add('fa-play')
+$('.article1 .plpa').on('click', function(){
+    var $ibutton = $(this).find('i')
+    if ( $ibutton.hasClass('fa-pause') ) { 
+        $('.slide-group').slick('slickPause')
+        $ibutton.removeClass('fa-pause').addClass('fa-play')
     } else {
-        $('.article1 .slide_group').slick('slickPlay')
-        ibtn[0].classList.remove('fa-play')
-        ibtn[0].classList.add('fa-pause')
+        $('.slide-group').slick('slickPlay')
+        $ibutton.removeClass('fa-play').addClass('fa-pause')
+    }
+})
+
+
+$('#header .open').on('click', function(){
+    $(this).removeClass('on')
+    $(this).next().next().next().next().addClass('on')
+    $(this).next().addClass('on')
+})
+
+$('#header .close').on('click', function(){
+    $(this).removeClass('on')
+    $(this).prev().prev().prev().prev().addClass('on')
+    $(this).prev().prev().prev().removeClass('on')
+    $(this).prev().find('.depth1 > li').removeClass('on')
+})
+
+// 여기서부터 resize 이벤트 발생 시 스크롤바 유무에 따른 상태 제어 프로그램
+var deviceSize = 1200;
+
+// 함수선언
+function scrollOX(status) {
+    $('html').css({
+        overflowY:status
+    })
+    var htmlWidth = $('html').width()
+    return htmlWidth
+}
+
+var swh = scrollOX('hidden')
+var sws = scrollOX('scroll')
+var swd = swh - sws
+if (swd>0) {
+    deviceSize = deviceSize - swd
+}
+
+
+// 함수선언
+function init(){
+    var ww = $(window).width()
+    if (ww>deviceSize && !$('html').hasClass('pc') ) {
+        $('html').addClass('pc').removeClass('mobile')
+        $('.search #sbox').removeClass('on')
+        $('.depth1 > li').removeClass('on')
+    } else if ( ww<=deviceSize && !$('html').hasClass('mobile') ) {
+        $('html').addClass('mobile').removeClass('pc')
+        $('#header .close').removeClass('on')
+        $('#header #nav').removeClass('on')
+        $('#header .open').addClass('on')
+        $('.search #sbox').removeClass('on')
+    }
+}
+
+// 함수호출
+init()
+
+$(window).on('resize', function(){
+    init()
+})
+// 여기까지
+
+
+$('.depth1 > li').hover(
+    function(){
+        if ( $('html').hasClass('pc') ) {
+            $(this).addClass('on')
+        }
+    },
+    function(){
+        if ( $('html').hasClass('pc') ) {
+            $(this).removeClass('on')
+        }
+    }
+)
+$('.depth1 > li').on('click', function(e){
+    if ( $('html').hasClass('mobile') ) {
+        e.preventDefault()
+        $(this).toggleClass('on')
+        $(this).siblings().removeClass('on')
+    }
+})
+
+
+
+
+
+$('.depth2 > li').on('click', function(e){
+    e.stopPropagation()
+})
+
+
+
+// 돋보기 클릭시 검색창 박스 열고닫기
+$('.search label').on('click', function(){
+    $(this).prev().toggleClass('on')
+})
+
+
+$(window).on('scroll', function(){
+    var sct = $(this).scrollTop()
+    if( sct>=10 && !$('#header').hasClass('on') ) {
+        $('#header').addClass('on')
+    } else if ( sct<10 && $('#header').hasClass('on') ) {
+        $('#header').removeClass('on')
     }
 })
